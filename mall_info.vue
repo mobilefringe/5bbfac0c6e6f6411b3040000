415dbc7cdc68dcc46c16dda90127ab3d36281ac2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-24 14:22:27 -0400

    Southland Mall 1.0  /mall_info.vue

--- mall_info.vue
+++ mall_info.vue
@@ -30,7 +30,6 @@
         			        </div>
         			    </div>
         			   <div class="col-md-6 col-lg-8" v-if="leasingSubpage">
-        			       
         			       <div v-if="pageContent">
         			            <div v-if="locale=='en-ca'" v-html="leasingSubpage.body"></div>
         			            <div v-else v-html="leasingSubpage.body_2"></div>
