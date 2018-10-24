3b1cc50273562602900b37f5c9da30ea81b5540b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-10-24 14:25:18 -0400

    Southland Mall 1.0  /mall_info.vue

--- mall_info.vue
+++ mall_info.vue
@@ -23,7 +23,7 @@
         				<v-select :options="dropDownSelect" :searchable="false" :on-change="selectPage" class="category-select" placeholder="Select a Page" inputId="pageSelect"></v-select>
         			</div>
         			<div class="row">
-        			    <div :class="{'col-sm-12': !leasingSubpage && leasingContent, 'col-sm-6': leasingSubpage}">
+        			    <div :class="{'col-sm-12': !leasingSubpage && leasingContent, 'col-sm-6': leasingSubpage && leasingContent}">
         			        <div v-if="pageContent">
         			            <div v-if="locale=='en-ca'" v-html="pageContent.body"></div>
         			            <div v-else v-html="pageContent.body_2"></div>
