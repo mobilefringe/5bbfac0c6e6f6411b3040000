37d7a98610eb638498c064ee7f5b30ff8ae5a573
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-02-24 11:35:33 -0500

    Southland Mall 1.1  /kids_club.vue

--- kids_club.vue
+++ kids_club.vue
@@ -6,7 +6,7 @@
                 <div class="page_header" v-if="pageBanner" v-bind:style="{ background: '#000 url(' + pageBanner.image_url + ')' }">
         			<div class="site_container">
         				<div class="header_content caps">
-        					<h1>{{ $t("programs_page.programs_header") }}</h1>
+        					<h1 class="banner-content">{{ $t("programs_page.programs_header") }}</h1>
         				</div>
         			</div>
         		</div>
