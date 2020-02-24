bb6afe691de5be1a43fcf98d5a4ae54119364b06
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-02-24 10:01:35 -0500

    Southland Mall 1.1  /home.vue

--- home.vue
+++ home.vue
@@ -20,7 +20,7 @@
         		<div class="site_container">
         		    <div class="visible_phone">
         		        <div class="hours_container home_hours margin_top_30">
-                            <h5 class="center caps" v-if="hour.is_open" v-for="hour in todaysHours">{{ property.name }} is open today:</h5>
+                            <div class="center caps h5" v-if="hour.is_open" v-for="hour in todaysHours">{{ property.name }} is open today:</div>
         		            <h5 class="center caps" v-else>{{ property.name }} is</h5>
                             <h3 class="center caps" v-if="hour.is_open" v-for="hour in todaysHours">{{hour.open_time | moment("h a", timezone)}} - {{hour.close_time | moment("h a", timezone)}}</h3>
                             <h3 class="center caps" v-else>Closed</h3>
