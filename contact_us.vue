74c185cd4e0d7f6a54842ca819d285606fac9cff
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-02-24 11:45:46 -0500

    Southland Mall 1.1  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -6,7 +6,7 @@
                 <div class="page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
         			<div class="site_container">
         				<div class="header_content caps">
-        					<h1>Contact Us</h1>
+        					<h1 class="banner-content">Contact Us</h1>
         				</div>
         			</div>
         		</div>
