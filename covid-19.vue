ee4b765581d02f2309ad1f354c245792c5694664
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-13 13:50:36 -0400

    Southland Mall 1.0  /covid-19.vue

--- covid-19.vue
+++ covid-19.vue
@@ -7,7 +7,7 @@
                     
                 <!--</div>-->
                 <div class="inside_header_background covid-background"  v-if="pageBanner" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
-                    <div class="center_text">
+                    <div class="site_container center_text">
                         <div class="page_container">
                             <h1><span>{{currentPage.title}} </span>Coronavirus information & Resources</h1>
                             
